385aefd13657565cb65266e775dd40e4dc3158a8
kuwa <83022348+kuwacom@users.noreply.github.com>
2023-01-02 21:43:54 +0900

    v1.1
    ヴの発音ができない問題の解
    エラーログ改善
    エラー時にtempが消えない問題の解決

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const fs = require("fs")
+const fs = require("fs");
 const axios = require('axios');
-const { exec } = require('child_process')
+const { exec } = require('child_process');
 const express = require("express");
 const iconv = require('iconv-lite');
 const app = express();
 const kLoger = require("./kloger");
+const { randomInt } = require("crypto");
 const loger = new kLoger({
     info: true,
     debug: true,
@@ -12,18 +13,18 @@ const loger = new kLoger({
 });
 
 // エラーハンドリング
-process.on("uncaughtException", function(err) {
-    loger.error(err)
-})
+process.on("uncaughtException", (err) => {
+    loger.error(err);
+});
 
 const server = app.listen(30596,'0.0.0.0', () => {
     loger.info("AquesTalk HTTP API bind on "+server.address().address+":"+ + server.address().port);
 });
 
 app.get("/synthesize", async (req, res, next) => {
-    let text = req.query.text
-    let type = req.query.type
-    let speed = req.query.speed
+    let text = req.query.text;
+    let type = req.query.type;
+    let speed = req.query.speed;
     if(!text){
         res.json({
             status:"error"
@@ -32,25 +33,26 @@ app.get("/synthesize", async (req, res, next) => {
     }
     if(!speed){speed = 1}
 
-    let url = encodeURI(`https://api.kuwa.app/voicevox/audio_query?text=${text}&speaker=0`)
+    let url = encodeURI(`https://api.kuwa.app/voicevox/audio_query?text=${text.replace("ヴ", "ボ")}&speaker=0`)
     let response = await axios.post(url);
-    text = response.data["kana"]
+    text = response.data["kana"];
+    console.log("\n"+text+"\n")
 
-    const inTEMP = "./temp/"+Math.floor(Math.random() * ( 999999 - 100000 ) + 100000)
-    const outTEMP = "./temp/"+Math.floor(Math.random() * ( 999999 - 100000 ) + 100000)
-    // fs.writeFileSync(inTEMP,text,'sft-js')
+    const inTEMP = "./temp/"+randomInt(100000,999999);
+    const outTEMP = "./temp/"+randomInt(100000,999999);
+
     const writer = fs.createWriteStream(inTEMP);
     writer.write(iconv.encode(text, "Shift_JIS"));
     writer.end();
-    const AquesTalkCMD = `AquesTalkCMD.exe ${type} ${speed*100} ${inTEMP} ${outTEMP}`
+    const AquesTalkCMD = `AquesTalkCMD.exe ${type} ${speed*100} ${inTEMP} ${outTEMP}`;
     exec(AquesTalkCMD, (err, stdout, stderr) => {
+        fs.unlinkSync(inTEMP);
         if (err) {
-            loger.error(err);
+            loger.error(`audio query => ${req.query.text}\nsynthesize => ${text} : ${type} : ${speed}\n ${err}`);
             return;
         }
-        loger.debug(`audio query => ${text} : ${type} : ${speed}`);
+        loger.debug(`audio query => ${req.query.text}\nsynthesize => ${text} : ${type} : ${speed}`);
         res.send(fs.readFileSync(outTEMP));
-        fs.unlinkSync(inTEMP);
         fs.unlinkSync(outTEMP);
-    })
+    });
 });
